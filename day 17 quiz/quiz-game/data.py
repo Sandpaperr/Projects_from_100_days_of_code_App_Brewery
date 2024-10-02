02aa3de9ecf2eae4576910f9abf532f225757e1b
Leandro Russo <sc21l2r@leeds.ac.uk>
2024-10-02 15:41:30 +0100

    adding day 17

---
+++ day 17 quiz/quiz-game/data.py
@@ -0,0 +1,58 @@
+question_data = [
+    {
+        "category": "History", "type": "boolean", "difficulty": "hard",
+        "question": "The Kingdom of Prussia briefly held land in Estonia.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Video Games", "type": "boolean",
+        "difficulty": "easy",
+        "question": "In 'Super Mario 64&', collecting 100 coins on a level will give you a 1-UP.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Books", "type": "boolean",
+        "difficulty": "easy",
+        "question": "The book 1984 was published in 1949.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Politics", "type": "boolean", "difficulty": "hard",
+        "question": "Joko Widodo has appeared in the cover of a TIME magazine.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Film", "type": "boolean",
+        "difficulty": "easy",
+        "question": "George Lucas directed the entire original Star Wars trilogy.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "General Knowledge", "type": "boolean",
+        "difficulty": "hard",
+        "question": "Stagecoach owned &quot;South West Trains&quot; before losing the rights to FirstGroup and MTR in March of 2017.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Video Games", "type": "boolean",
+        "difficulty": "medium",
+        "question": "In &quot;Starbound&quot;, the track played by the Decorated Music Box is named &quot;Atlas&quot;.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "History", "type": "boolean", "difficulty": "easy",
+        "question": "Thomas Crapper was a plumber who invented the flushing toilet.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Japanese Anime & Manga",
+        "type": "boolean", "difficulty": "hard",
+        "question": "The character Plum from &quot;No Game No Life&quot; is a girl.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "History", "type": "boolean", "difficulty": "hard",
+        "question": "The man that shot Alexander Hamilton was named Aaron Burr.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    }
+]
---
+++ day 17 quiz/quiz-game/main.py
@@ -0,0 +1,28 @@
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+def quiz_game():
+
+    question_bank = []
+
+    # Create quiz bank
+    for dictionary in question_data:
+        text = dictionary["question"]
+        answer = dictionary["correct_answer"]
+        question_obj = Question(text, answer)
+
+        question_bank.append(question_obj)
+
+    quiz = QuizBrain(question_bank)
+
+    while quiz.has_still_questions():
+        quiz.next_question()
+
+    print("You've completed the quiz")
+    print(f"Your final score is {quiz.score}/{quiz.question_number}")
+
+
+
+
+quiz_game()
