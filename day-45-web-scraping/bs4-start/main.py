e052c410d014cf51b90a43468f4773cb6387d65c
Leandro Russo <russo.leand@gmail.com>
2024-10-16 18:51:30 +0100

    added code to scrape links and titles

--- day-45-web-scraping/bs4-start/main.py
+++ day-45-web-scraping/bs4-start/main.py
@@ -2,8 +2,9 @@ from bs4 import BeautifulSoup
 import requests
 
 response = requests.get("https://news.ycombinator.com/news")
-print(response.text)
+
 #get hold of title and link of the news with most points
 soup = BeautifulSoup(response.text, "html.parser")
 
-links_and_titles = soup.find_all(name="a")
+links_and_titles = soup.select(selector="span.titleline a")
+print(links_and_titles[0])
