345c89aed3eb9229456c60f40b7362e2a7c7442f
Leandro Russo <russo.leand@gmail.com>
2024-10-17 12:04:18 +0100

    added code to scrape scores

--- day-45-web-scraping/bs4-start/main.py
+++ day-45-web-scraping/bs4-start/main.py
@@ -1,10 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
 
+def YCombinator_webiste_scrape(soup: BeautifulSoup):
+
+    links_and_titles_raw = soup.select(selector="span.titleline a")
+    score_raw = soup.select(selector="span.score")
+    scores = [i.get_text() for i in score_raw]
+    print(scores)
+    
+
+
 response = requests.get("https://news.ycombinator.com/news")
 
 #get hold of title and link of the news with most points
 soup = BeautifulSoup(response.text, "html.parser")
+YCombinator_webiste_scrape(soup)
 
-links_and_titles = soup.select(selector="span.titleline a")
-print(links_and_titles[0])
