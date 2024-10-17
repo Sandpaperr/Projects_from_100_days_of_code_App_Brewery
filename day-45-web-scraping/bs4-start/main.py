32f9e9be1f9c8ece36c641d5de75ff22c6b91d77
Leandro Russo <russo.leand@gmail.com>
2024-10-17 15:40:53 +0100

    created dict for storing scores, titles and links.

--- day-45-web-scraping/bs4-start/main.py
+++ day-45-web-scraping/bs4-start/main.py
@@ -3,10 +3,37 @@ import requests
 
 def YCombinator_webiste_scrape(soup: BeautifulSoup):
 
-    links_and_titles_raw = soup.select(selector="span.titleline a")
-    score_raw = soup.select(selector="span.score")
-    scores = [i.get_text() for i in score_raw]
-    print(scores)
+    links_and_titles_raw =[i.find(name="a") for i in soup.select(selector="span.titleline")] 
+    links = [i.get("href") for i in links_and_titles_raw]
+    titles = [i.get_text() for i in links_and_titles_raw]
+
+    # every title has a subtext
+    sublines = [i for i in soup.select(selector="td.subtext")]
+    scores = []
+    
+    for raw in sublines:
+        #only the titles that have score has a subline
+        subline = raw.find(name="span", class_="subline")
+
+        if subline is None:
+            scores.append(0)
+        else:
+            scores.append(int(subline.find(name="span", class_="score").get_text().split()[0]))
+
+
+    dict_for_scores_titles_links = {}
+
+    for i in range(len(scores)):
+        dict_for_scores_titles_links[scores[i]] = [titles[i], links[i]]
+
+
+    max_score = max(dict_for_scores_titles_links)
+    print(max_score , dict_for_scores_titles_links[max_score])
+
+    
+    
+    
+
     
 
 
