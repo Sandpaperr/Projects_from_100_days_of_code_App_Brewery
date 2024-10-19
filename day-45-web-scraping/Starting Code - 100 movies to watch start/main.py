f1c3bd70f721c801e2b0a92fb7edab14f345b726
Leandro Russo <russo.leand@gmail.com>
2024-10-19 20:26:32 +0100

    finished day 45

--- day-45-web-scraping/Starting Code - 100 movies to watch start/main.py
+++ day-45-web-scraping/Starting Code - 100 movies to watch start/main.py
@@ -5,4 +5,18 @@ URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/m
 
 # Write your code below this line 👇
 
+# Make it a bit more safe, check if response was good first
+soup = BeautifulSoup(requests.get(URL).text, "html.parser")
+
+titles = soup.select("h3.title")
+
+with open("./day-45-web-scraping\Starting Code - 100 movies to watch start\movies.txt", "w", encoding="utf-8") as file:
+    for title in  reversed(titles):
+        title_good = title.get_text()
+        file.write(title_good)
+        file.write("\n")
+
+
+
+
 
