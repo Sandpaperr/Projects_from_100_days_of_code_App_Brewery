1ef08c8c97a964b3fcda2bb48f5675ffc3879815
Leandro Russo <russo.leand@gmail.com>
2024-10-21 07:57:13 +0100

    added request

--- day-46-time-travel/main.py
+++ day-46-time-travel/main.py
@@ -1,5 +1,14 @@
-
+from bs4 import BeautifulSoup
+import requests
 
 header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
 
-input("Which Year would you like to travel to? YYYY-MM-DD")
+date = input("Which Year would you like to travel to? YYYY-MM-DD: ")
+
+#TODO: Make sanity checks on the input
+
+url = "https://www.billboard.com/charts/hot-100/" + date
+
+response = requests.get(url=url, headers=header)
+
+soup = BeautifulSoup(response.text, "html.parser")
