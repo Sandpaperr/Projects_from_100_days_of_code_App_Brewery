428a1536b809bb30bd1a8f1aabeade279fdc9158
Leandro Russo <russo.leand@gmail.com>
2024-10-27 14:03:28 +0000

    changed the url to use the amazon live website

--- day-47-price-tracker/main.py
+++ day-47-price-tracker/main.py
@@ -12,11 +12,28 @@ RECEIVER_EMAIL = os.getenv("RECEIVER_EMAIL")
 SMTP_ADDRESS = os.getenv("SMTP_ADDRESS")
 PASSWORD = os.getenv("PASSWORD")
 
+header = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Priority": "u=0, i",
+    "Sec-Ch-Ua": "\"Chromium\";v=\"130\", \"Google Chrome\";v=\"130\", \"Not?A_Brand\";v=\"99\"",
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": "\"Windows\"",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "cross-site",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
+  }
+
+
 
 #Static URL for amazon
-url = "https://appbrewery.github.io/instant_pot/"
+url = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
 
-response = requests.get(url=url)
+response = requests.get(url=url, headers=header)
 
 soup = BeautifulSoup(response.text, "html.parser")
 
