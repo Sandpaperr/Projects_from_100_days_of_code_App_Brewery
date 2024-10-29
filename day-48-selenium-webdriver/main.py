a40b1418d20bb061998627a3e195e6a871cfbfa1
Leandro Russo <russo.leand@gmail.com>
2024-10-29 14:35:35 +0000

    scraping amazon website using selenium

--- day-48-selenium-webdriver/main.py
+++ day-48-selenium-webdriver/main.py
@@ -1,4 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.get("https://www.amazon.com")
+#Keep the Chrome browser open after program finishes
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+#whitout this line amazon detect the robot
+chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36")
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6")
+
+price_whole = driver.find_element(By.CLASS_NAME, value="a-price-whole")
+price_cents = driver.find_element(By.CLASS_NAME, value="a-price-fraction")
+
+print(f"the price is {price_whole.text}.{price_cents.text}")
+
+#to close the single page browser
+driver.close()
+#close all tabs 
+driver.quit()
