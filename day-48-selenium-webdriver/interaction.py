6c96fe7f407d23a1ac6b8312b7fe2ce13bf335c1
Leandro Russo <russo.leand@gmail.com>
2024-11-01 16:41:23 +0000

    finished the tutorials for day 48

--- day-48-selenium-webdriver/interaction.py
+++ day-48-selenium-webdriver/interaction.py
@@ -11,24 +11,44 @@ chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64
 
 driver = webdriver.Chrome(options=chrome_options)
 
-#get hold of number of articles form wikipedia and click on the link
-driver.get("https://en.wikipedia.org/wiki/Main_Page")
-n_of_articles = driver.find_element(By.CSS_SELECTOR, value='#articlecount a')
-#n_of_articles.click()
-print(n_of_articles.text)
 
-#another way to click on link
-all_portals = driver.find_element(By.LINK_TEXT, value="Content portals")
-#all_portals.click()
+#Trying methods for interacting with a website
+# #get hold of number of articles form wikipedia and click on the link
+# driver.get("https://en.wikipedia.org/wiki/Main_Page")
+# driver.maximize_window()
+# n_of_articles = driver.find_element(By.CSS_SELECTOR, value='#articlecount a')
+# #n_of_articles.click()
+# print(n_of_articles.text)
 
-# write and search in search bar
-search_bar = driver.find_element(By.NAME, value="search")
+# #another way to click on link
+# all_portals = driver.find_element(By.LINK_TEXT, value="Content portals")
+# #all_portals.click()
 
-#send keys
-search_bar.send_keys("Python")
 
-#send enter -> same method for all special characters
-search_bar.send_keys(Keys.ENTER)
+# # write and search in search bar
+# search_bar = driver.find_element(By.NAME, value='search')
+
+# #send keys. send enter -> same method for all special characters
+
+# search_bar.send_keys("Python", Keys.ENTER)
+
+
+#Sign online form
+
+driver.get("https://secure-retreat-92358.herokuapp.com/")
+driver.maximize_window()
+
+name_field = driver.find_element(By.NAME, value="fName")
+surname_field = driver.find_element(By.NAME, value="lName")
+email_field = driver.find_element(By.NAME, value="email")
+
+name_field.send_keys("test")
+surname_field.send_keys("testttttt")
+email_field.send_keys("test_test@gmail.com")
+
+submit = driver.find_element(By.CLASS_NAME, value="btn")
+
+submit.click()
 
 
 #to close the single page browser
