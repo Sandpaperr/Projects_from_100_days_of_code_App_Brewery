92cff8b45677f6f578e1960b8490325c9894184a
Leandro Russo <russo.leand@gmail.com>
2024-11-02 11:57:29 +0000

    started day 49

---
+++ day-49-automatic-apply-linkeding/main.py
@@ -0,0 +1,52 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import StaleElementReferenceException, NoSuchElementException, NoSuchWindowException
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+MY_EMAIL = os.getenv("MY_EMAIL_LINKEDIN")
+MY_PASSWORD = os.getenv("MY_PASSWORD_LINKEDIN")
+
+#Keep the Chrome browser open after program finishes
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+
+chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36")
+
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get(r"https://www.linkedin.com/jobs/search/?currentJobId=4034296435&f_AL=true&geoId=102943586&keywords=python%20developer&origin=JOB_SEARCH_PAGE_LOCATION_AUTOCOMPLETE&refresh=true")
+
+redirected = True
+while redirected:
+    try:
+        time.sleep(2)
+        sign_in_button = driver.find_element(By.CSS_SELECTOR, value="button.sign-in-modal__outlet-btn.cursor-pointer.btn-md.btn-primary")
+        sign_in_button.click()
+        redirected = False
+        
+    except NoSuchElementException:
+        time.sleep(15)
+        driver.get(r"https://www.linkedin.com/jobs/search/?currentJobId=4034296435&f_AL=true&geoId=102943586&keywords=python%20developer&origin=JOB_SEARCH_PAGE_LOCATION_AUTOCOMPLETE&refresh=true")
+        redirected = True
+
+time.sleep(5)
+email_input = driver.find_element(By.ID, value="base-sign-in-modal_session_key")
+password_input = driver.find_element(By.ID, value="base-sign-in-modal_session_password")
+
+#input email and password to sign in
+email_input.send_keys(MY_EMAIL)
+password_input.send_keys(MY_PASSWORD)
+sign_in_button = driver.find_element(By.CSS_SELECTOR, value="button.btn-md.btn-primary.flex-shrink-0.cursor-pointer.sign-in-form__submit-btn--full-width")
+sign_in_button.click()
+
+#get all the job available
+time.sleep(5)
+all_listings = driver.find_elements(by=By.CSS_SELECTOR, value=".job-card-container--clickable")
+
+#TODO: create a file where I put all the jobs I have applied for 
