a292d4d569e32898cb59a4513c9668458e3dfcbc
Leandro Russo <russo.leand@gmail.com>
2024-11-06 16:56:04 +0000

    added decorations

--- day-54-into-to-Flask/hello.py
+++ day-54-into-to-Flask/hello.py
@@ -1,5 +1,25 @@
 from flask import Flask
 
+#decorator for makking bold the text returned by another function
+def make_bold(function):
+    def wrapped_function():
+        text = function()
+        return f"<b>{text}</b>"
+    return wrapped_function
+
+def make_emphasis(function):
+    def wrapped_function():
+        text = function()
+        return f"<em>{text}</em>"
+    return wrapped_function
+
+def make_underline(function):
+    def wrapped_function():
+        text = function()
+        return f"<u>{text}</u>"
+    return wrapped_function
+
+
 #__name__ is a special attribute that tells us the name of the current class, function, method or descriptor  
 app = Flask(__name__)
 
@@ -8,6 +28,9 @@ app = Flask(__name__)
 #when the user hits up this route ("/")do hello_word
 #@app.route is a python decorator -> function that gives additional functionality to an existent function
 @app.route("/")
+@make_bold
+@make_underline
+@make_emphasis
 def hello_world():
     return "<p>Hello, World!</p>"
 
@@ -15,6 +38,24 @@ def hello_world():
 def say_bye():
     return "Bye"
 
+@app.route("/username/<name>")
+def greet(name):
+    return f"greeting {name}"
+
+@app.route("/ub")
+def greet_html():
+    return f"<h1 style='text-align: center'>greeting </h1><p>this is a paragraph</p>"
+
+
+#path capture everything between username and /1/ll...
+@app.route("/username/<path:path>/1/ll/<int:number>/alba")
+def greet_path(path, number):
+    return f"greeting {path} {number}"
+
+
+
+
+
 #Common way to use __name__
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
