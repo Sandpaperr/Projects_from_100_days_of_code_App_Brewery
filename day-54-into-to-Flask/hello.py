6dcda0725e12ab1f07471bc4cb4dd0d9009a1f1f
Leandro Russo <russo.leand@gmail.com>
2024-11-06 11:19:37 +0000

    started flask exercise

---
+++ day-54-into-to-Flask/hello.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+#__name__ is a special attribute that tells us the name of the current class, function, method or descriptor  
+app = Flask(__name__)
+
+#functions are first-class objects, can be passed around as arguments 
+
+#when the user hits up this route ("/")do hello_word
+#@app.route is a python decorator -> function that gives additional functionality to an existent function
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+#Common way to use __name__
+if __name__ == "__main__":
+    app.run()
