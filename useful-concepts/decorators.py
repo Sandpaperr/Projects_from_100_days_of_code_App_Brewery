e87ff0c65b28a7349dbfff04aca2cedd8e74605b
Leandro Russo <russo.leand@gmail.com>
2024-11-06 12:36:54 +0000

    added use of function args from the decorator

--- useful-concepts/decorators.py
+++ useful-concepts/decorators.py
@@ -31,3 +31,25 @@ say_greetings()
 #The "proper" line is this
 decorated_function = delay_decorator(say_greetings)
 decorated_function()
+
+
+#Usings variables in a function from the wrapper function
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.is_logged_in = False
+
+def is_authenticated_decorator(function):
+    def wrapper(*args, **kwargs):
+        if args[0].is_logged_in == True:
+            function(args[0])
+
+    return wrapper
+
+@is_authenticated_decorator
+def create_blog_post(user):
+    print(f"This is {user.name}'s blog post")
+
+new_user = User("Leandro")
+new_user.is_logged_in = True
+create_blog_post(new_user)
